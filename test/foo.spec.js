54cd760f4deb6c101dae139182a88bebe8a06456
Eric P. Katz <ericpkatz@gmail.com>
2016-03-23 06:00:13 -0400

    another test

--- test/foo.spec.js
+++ test/foo.spec.js
@@ -1,8 +1,20 @@
-function Foo(){
-
+function Foo(bar){
+  this.bar = bar;
 }
 describe('Foo', function(){
   it('exists', function(){
     expect(Foo).to.be.ok;
+  });
+
+  describe('creating a Foo', function(){
+    describe('when bar is set to buzz', function(){
+      var foo;
+      beforeEach(function(){
+        foo = new Foo('buzz');
+      });
+      it('foo.bar is buzz', function(){
+        expect(foo.bar).to.equal('buzz');
+      });
+    });
   });
 });
